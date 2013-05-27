31c49e8118320ed6109587280ccbf6c2b8331c4b
Pelle Nilsson <perni@lysator.liu.se>
2013-05-27 23:48:13 +0200

    Raise error if attempting to us RTF, as it is not yet implemented.

--- rtf.py
+++ rtf.py
@@ -4,3 +4,5 @@ class RtfFormat (OutputFormat):
     def __init__(self):
         super(RtfFormat, self).__init__('rtf', 'Rich Text Format')
 
+    def write(self, book, output):
+        raise Exception("RTF output format not yet supported. :(")
