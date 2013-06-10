116ca6fad199906af6e8d358c58c0e06bb2c188e
Pelle Nilsson <perni@lysator.liu.se>
2013-06-10 21:43:03 +0200

    Include templates from a template. Should make it much easier to override specific parts of default templates.

--- templates.py
+++ templates.py
@@ -1,6 +1,10 @@
+import re
 import os
 import os.path
 import sys
+
+PREPROCESS_RE = re.compile("^\s*#")
+INCLUDE_RE = re.compile('^\s*#include\s*"(\w+)"')
 
 class Templates (object):
     def __init__(self, templatedirs, extension):
@@ -23,10 +27,26 @@ class Templates (object):
     def get_in(self, templatedir, name):
         filename = self.get_template_filename(templatedir, name)
         f = open(filename, "r")
-        template = f.read()
+        template = self.read_template(f);
         f.close()
         self.cached_templates[name] = template
         return template
+
+    def read_template(self, f):
+        res = ""
+        for line in f.readlines():
+            if PREPROCESS_RE.match(line):
+                res += self.preprocessline(line)
+            else:
+                res += line
+        return res
+
+    def preprocessline(self, line):
+        m = INCLUDE_RE.match(line)
+        if m:
+            return self.get(m.group(1))
+        else:
+            raise Exception("Bad preprocessor line '%s' in template." % line)
 
     def get_template_filename(self, templatedir, name):
         return os.path.join(templatedir,
