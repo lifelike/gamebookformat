63041a5e62573a442e8a43b43e4c2a9c3c522e8e
Pelle Nilsson <perni@lysator.liu.se>
2014-09-23 23:15:00 +0200

    checkgamebook.py now can find unreachable sections in books.

--- checkgamebook.py
+++ checkgamebook.py
@@ -35,8 +35,63 @@ import json
 
 USAGE = "usage: %prog [options] inputfile(s)... outputfile"
 
+def find_section_nr_names(sections):
+    section_nr_to_name = {}
+    for section_name,section_contents in sections.iteritems():
+        section_nr_to_name[section_contents["nr"]] = section_name
+    return section_nr_to_name
+
+def find_references(sections):
+    section_nr_to_name = find_section_nr_names(sections)
+    references = {}
+    for section_name,section_contents in sections.iteritems():
+        references_from_section = []
+        for t in section_contents["text"]:
+            if isinstance(t, dict) and "reference" in t:
+                ref = section_nr_to_name[int(t["reference"])]
+                references_from_section.append(ref)
+        references[section_name] = references_from_section
+    return references
+
+def traverse(sections, references, function, data):
+    stack = ["start"]
+    visited = set()
+    while len(stack) > 0:
+        section = stack.pop()
+        function(section, data)
+        visited.add(section)
+        for reference in references[section]:
+            if reference not in visited:
+                stack.append(reference)
+
+def traverse_add(section, data):
+    data.add(section)
+
+found_errors = False # yay, global state
+def check_error(msg):
+    print "%s: %s" % (sys.argv[1], msg)
+    global found_errors
+    found_errors = True
+
+def check_all_sections_can_be_reached_in_theory(sections, references):
+    reached = set()
+    traverse(sections, references, traverse_add, reached)
+    for section in sections:
+        if section not in reached:
+            check_error("Could not reach section '%s' from start." % section)
+
 def check_gamebook(inputfilename):
     book = json.load(open(inputfilename))
+    sections = book["sections"]
+    if not "start" in sections:
+        check_error("No start section found. Cancelling checks.")
+        sys.exit(1)
+    del sections["IGNORE-debug-json-padding-IGNORE"]
+    for section in sections.keys():
+        if section.startswith("empty-"):
+            del sections[section]
+    references = find_references(sections)
+    check_all_sections_can_be_reached_in_theory(sections, references)
 
 if __name__ == '__main__':
     import argparse
@@ -46,3 +101,7 @@ if __name__ == '__main__':
                     help='input gamebook debug file (eg test.debug)')
     args = ap.parse_args()
     check_gamebook(args.inputfile)
+    if found_errors:
+        sys.exit(1)
+    else:
+        sys.exit(0)
